1d04ac3320598a504965f309847ea5a1aad66f53
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-22 15:19:30 +0200

    finish milestone 1

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            todo: [
+                {
+                   text:  "Quarterly Newsletter",
+                   done: true,
+                   fonte: "Editorial"
+                },
+                {
+                    text:  "Recruiting blog post",
+                    done: true,
+                    fonte: "Editorial"
+                 },
+                 {
+                    text:  "Mobile app luaunch",
+                    done: true,
+                    fonte: "Q1 Goals"
+                 },
+                 {
+                    text:  "Interview John H.",
+                    done: false,
+                    fonte: "Recruiting"
+                 },
+                 {
+                    text:  "Summit update to mobile storefronts",
+                    done: true,
+                    fonte: "Mobile"
+                 },
+                 {
+                    text:  "Schedule meeting with Alex",
+                    done: false,
+                 },
+                 {
+                    text:  "Homepage refresh",
+                    done: true,
+                    fonte: "Website"
+                 },
+                 {
+                    text:  "Onboard new Sales team members",
+                    done: true,
+                    fonte: "Sales"
+                 },
+                 {
+                    text:  "Review editorial calendar",
+                    done: true,
+                    fonte: "Editorial"
+                 },
+            ],
+            selectedIndex: 0
+        };
+    },
+    methods: {
+        select(index){
+            this.selectedIndex = index;
+        },
+        addClass(dati){
+            return {
+                
+            }
+        }
+    }
+}).mount("#app");
